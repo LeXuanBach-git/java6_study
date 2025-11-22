c11bea647cd474f5d4c51000903f163cadc7bf58
LeXuanBach-git <xuanbachle007@gmail.com>
2025-11-22 17:36:36 +0700

    Security InMemoryUserDetailsManager

---
+++ src/main/java/org/example/java6_thuchanh/config/SpringSecurityCofig.java
@@ -0,0 +1,55 @@
+package org.example.java6_thuchanh.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SpringSecurityCofig {
+
+
+    // ma hoa pass
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        // be khoa, cho phep CRUD
+        http.csrf(csrsConfigurer -> csrsConfigurer.disable())
+                .authorizeHttpRequests(authorrize -> {
+                    authorrize.anyRequest().authenticated();
+                })
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        // custom user
+        UserDetails customUser = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("123456"))
+                .roles("USER")
+                .build();
+
+        UserDetails customAdmin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("123456"))
+                .roles("ADMIN", "USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(customUser, customAdmin);
+    }
+}
+
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,11 @@ INSERT INTO todos(title, description, completed) VALUES('Todo 2', 'Description 2
 
 INSERT INTO books (title, author, price, isbn)
 VALUES ('Java Programming Basics', 'Nguyen Van A', 199000, 'ISBN-001');
-
 INSERT INTO books (title, author, price, isbn)
 VALUES ('Spring Boot in Action', 'Tran Thi B', 259000, 'ISBN-002');
-
 INSERT INTO books (title, author, price, isbn)
 VALUES ('Database Design', 'Le Van C', 179000, 'ISBN-003');
+
+INSERT INTO student VALUES ('ten 1', 'dia chi 1'),
+                           ('ten 2', 'dia chi 2');
+
